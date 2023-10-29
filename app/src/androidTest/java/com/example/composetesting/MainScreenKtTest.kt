9e697041fdf038a6b285b662125f726f41b106fc
Parkduksung <duksung1234@naver.com>
2023-10-30 00:33:18 +0900

    button test

--- app/src/androidTest/java/com/example/composetesting/MainScreenKtTest.kt
+++ app/src/androidTest/java/com/example/composetesting/MainScreenKtTest.kt
@@ -1,9 +1,9 @@
 package com.example.composetesting
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
 
@@ -23,11 +23,40 @@ class MainScreenKtTest {
     }
 
     @Test
-    fun initScreenTextTest(){
+    fun initScreenTextTest() {
 
         //find node by text and assert
-        composeTestRule.onNodeWithText("Hello Test!").assertExists()
+        composeTestRule.onNodeWithText("Hello Test! click count : 0").assertExists()
 
+    }
+
+
+    @Test
+    fun clickButtonTest1() {
+
+            //find node by tag and click
+            composeTestRule.onNodeWithTag("sample_button").performClick()
+
+            //find node by text and assert
+            composeTestRule.onNodeWithText("Hello Test! click count : 1").assertExists()
+    }
+
+
+    @Test
+    fun clickButtonTest2() {
+
+        //find node by tag and click
+        composeTestRule
+            .onNodeWithTag("sample_button")
+
+        val repeatCount = 5
+
+        repeat(repeatCount){
+            composeTestRule.onNodeWithTag("sample_button").performClick()
+        }
+
+        //find node by text and assert
+        composeTestRule.onNodeWithText("Hello Test! click count : $repeatCount").assertExists()
     }
 
 }
--- app/src/main/java/com/example/composetesting/MainScreen.kt
+++ app/src/main/java/com/example/composetesting/MainScreen.kt
@@ -2,23 +2,47 @@ package com.example.composetesting
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
+import androidx.compose.ui.unit.dp
 
 
 //cmd+shift+T => Test
 @Composable
 fun SampleScreen() {
 
+    var clickNum by remember { mutableStateOf(0) }
+
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
 
         //add semantics
-        Text(text = "Hello Test!", modifier = Modifier.semantics { testTag = "sample_text" })
+        Text(
+            text = "Hello Test! click count : $clickNum",
+            modifier = Modifier.semantics { testTag = "sample_text" })
 
+
+        Button(
+            onClick = {
+                clickNum++
+            },
+            modifier = Modifier
+                .testTag("sample_button")
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 20.dp)
+        ) {
+            Text(text = "Click Me!")
+        }
     }
 
 }
