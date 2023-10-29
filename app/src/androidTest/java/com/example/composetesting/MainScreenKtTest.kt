43df69334062aa9889b5cc54dd85b29a74eaf23f
Parkduksung <duksung1234@naver.com>
2023-10-29 21:41:59 +0900

    text test

--- app/src/androidTest/java/com/example/composetesting/MainScreenKtTest.kt
+++ app/src/androidTest/java/com/example/composetesting/MainScreenKtTest.kt
@@ -2,6 +2,7 @@ package com.example.composetesting
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
 
@@ -11,6 +12,7 @@ class MainScreenKtTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
+
 
     @Test
     fun initScreenTest() {
@@ -22,7 +24,17 @@ class MainScreenKtTest {
 
         //find node by tag and assert
         composeTestRule.onNodeWithTag("sample_text").assertExists()
+    }
 
+    @Test
+    fun initScreenTextTest(){
+
+        composeTestRule.setContent {
+            SampleScreen()
+        }
+
+        //find node by text and assert
+        composeTestRule.onNodeWithText("Hello Test!").assertExists()
 
     }
 
