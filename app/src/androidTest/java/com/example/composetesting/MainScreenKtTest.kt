d7b4d9ab79c7fa7cf1cee84f87861b79ebb87eda
Parkduksung <duksung1234@naver.com>
2023-10-31 20:46:42 +0900

    Red - testcase 추가.

--- app/src/androidTest/java/com/example/composetesting/MainScreenKtTest.kt
+++ app/src/androidTest/java/com/example/composetesting/MainScreenKtTest.kt
@@ -1,9 +1,12 @@
 package com.example.composetesting
 
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,7 +15,17 @@ class MainScreenKtTest {
 
 
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<MainActivity>()
+    val composeTestRule = createComposeRule()
+
+    private lateinit var navController: NavController
+
+    @Before
+    fun setUp(){
+        composeTestRule.setContent {
+            navController = rememberNavController()
+            MainScreen(navController = navController)
+        }
+    }
 
 
     @Test
@@ -59,4 +72,22 @@ class MainScreenKtTest {
         composeTestRule.onNodeWithText("Hello Test! click count : $repeatCount").assertExists()
     }
 
+    @Test
+    fun `버튼을_10번_초과_눌렀을_시_서브화면으로_이동되어야한다`() {
+
+        val repeatCount = 10
+
+        repeat(repeatCount){
+            composeTestRule.onNodeWithTag("sample_button").performClick()
+        }
+
+        //find node by text and assert
+        composeTestRule.onNodeWithText("Hello Test! click count : $repeatCount").assertExists()
+
+        //find node by tag and click
+        composeTestRule.onNodeWithTag("sample_button").performClick()
+
+        //find node by text and assert
+        composeTestRule.onNodeWithText("Hello SubScreen!").assertExists()
+    }
 }
